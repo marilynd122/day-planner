2083efeaa765a45550524afc458b25585b313a7b
wwhub <wendy98289@gmail.com>
2019-10-27 13:36:29 -0400

    adjust row to center of page

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var todayDate = moment().format('dddd, MMMM Do YYYY');
 dateParagraph.text(todayDate);
 });
 
-
+//button to clear local storage and refresh the page
 $("#clear").click(function() {
     localStorage.clear();
     location.reload()
